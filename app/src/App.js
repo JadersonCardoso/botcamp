4402cb3b09d6c2a1b381d44de03bb937f1e9255c
Jaderson Brandão <jaderson.brandao@gmail.com>
2019-04-04 03:04:47 -0300

    Adicionando styled-components e modificando o componente LoginWrapper

--- app/src/App.js
+++ app/src/App.js
@@ -2,16 +2,19 @@ import React, { Component } from 'react'
 
 import BtnBotcamp from './components/BtnBotCamp'
 import LogoBotcamp from './components/LogoBotcamp'
+import LoginWrapper from './components/LoginWrapper'
 
 import './styles/reset.css'
 
 class App extends Component {
     render() {
         return(
-         <>   
-            <LogoBotcamp/>
-            <BtnBotcamp />
-         </>   
+         
+            <LoginWrapper>
+                <LogoBotcamp/>
+                <BtnBotcamp />
+            </LoginWrapper>
+         
         )  
         
     }
