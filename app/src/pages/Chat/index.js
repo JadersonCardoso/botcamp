51421ba5cde6231a1d234adcf4216b15850fedb7
Jaderson Brandão <jaderson.brandao@gmail.com>
2019-04-08 23:46:17 -0300

    Aula04 - Usar o componente LogoBotcamp

--- app/src/pages/Chat/index.js
+++ app/src/pages/Chat/index.js
@@ -3,11 +3,11 @@ import React from 'react'
 import LogoChat from '../../components/LogoChat';
 import IconLogout from '../../components/IconLogout';
 import HeaderChat from '../../components/HeaderChat';
-
+import LogoBotcamp from '../../components/LogoBotcamp'
 const Chat = () => (
     <>
         <HeaderChat >
-            <LogoChat />
+            <LogoBotcamp />
             <IconLogout />
         </HeaderChat>
    </> 
