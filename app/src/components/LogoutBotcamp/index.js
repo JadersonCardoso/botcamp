e553fb2611e64aee15c58467cf0aaa924dbeecc9
Jaderson Brandão <jaderson.brandao@gmail.com>
2019-04-08 23:58:56 -0300

    Aula46 - Criando componente LogoutBotcamp

---
+++ app/src/components/LogoutBotcamp/index.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+import logout from '../../imgs/iconfinder_logout_4115235.png'
+
+const LogoutBotcamp = styled.img.attrs({
+    src: logout,
+    alt: 'Icon logout chat'
+})`
+    width: 30px;
+    height: 30px;
+    object-fit: contain;
+`
+export default LogoutBotcamp
--- app/src/pages/Chat/index.js
+++ app/src/pages/Chat/index.js
@@ -1,14 +1,13 @@
 import React from 'react'
 
-import LogoChat from '../../components/LogoChat';
-import IconLogout from '../../components/IconLogout';
+import LogoutBotcamp from '../../components/LogoutBotcamp';
 import HeaderChat from '../../components/HeaderChat';
 import LogoBotcamp from '../../components/LogoBotcamp'
 const Chat = () => (
     <>
         <HeaderChat >
             <LogoBotcamp />
-            <IconLogout />
+            <LogoutBotcamp />
         </HeaderChat>
    </> 
 )
