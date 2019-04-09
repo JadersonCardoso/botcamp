9823b84c9d4590c3bdcc4365057e43d8fd37a196
Jaderson Brandão <jaderson.brandao@gmail.com>
2019-04-09 00:18:30 -0300

    Aula04 48 Adicionar respiro no HeaderWrapper

--- app/src/components/HeaderWrapper/index.js
+++ app/src/components/HeaderWrapper/index.js
@@ -1,9 +1,8 @@
 import styled from 'styled-components'
 
 const HeaderWrapper = styled.header`
-    width: 375px;
-    height: 66px;
-    box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.25);
     background-color: #26265e;
+    padding: 10px 16px;
+
 `
 export default HeaderWrapper
